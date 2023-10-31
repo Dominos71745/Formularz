d7aae58579e64c9c0d8340bed440ac55fcb0dfa4
Dominos71745 <domistu@poczta.fm>
2023-10-31 22:36:59 +0100

    Add project files.

---
+++ Controllers/DaneController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Formularz.Models;
+
+namespace Formularz.Controllers
+{
+    public class DaneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form(Dane dane)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content("Rejestracja zakończona pomyślnie");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Wynik(Dane dane)
+        {
+            return View(dane);
+        }
+    }
+}
